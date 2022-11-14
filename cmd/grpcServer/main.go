a523bdb8d7db638e56e3b64b1ec84ed583f34fa9
Lucas Coronel <lucas.coronel@platformbuilders.io>
2022-11-14 08:31:33 -0300

    feat: grpc server

---
+++ cmd/grpcServer/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"database/sql"
+	"net"
+
+	"github.com/lccoronel/gRPC-full-cycle/internal/database"
+	"github.com/lccoronel/gRPC-full-cycle/internal/pb"
+	"github.com/lccoronel/gRPC-full-cycle/internal/services"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "./db.sqlite")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	categoryDB := database.NewCategory(db)
+	categoryService := services.NewCategoryService(*categoryDB)
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterCategoryServiceServer(grpcServer, categoryService)
+	reflection.Register(grpcServer)
+
+	lis, err := net.Listen("tcp", ":5051")
+	if err != nil {
+		panic(err)
+	}
+
+	if err := grpcServer.Serve(lis); err != nil {
+		panic(err)
+	}
+}
